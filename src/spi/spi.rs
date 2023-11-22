984fbf3931109b054faa8d0dcf49fee18c95e9e0
Serena <23511780+SploxFox@users.noreply.github.com>
2023-11-21 19:48:19 -0500

    Made it generic

--- src/spi/spi.rs
+++ src/spi/spi.rs
@@ -1,10 +1,11 @@
+use embassy_stm32::Peripheral;
 use embassy_stm32::peripherals::*;
 use embassy_stm32::spi as em_spi;
 
-pub type EmbassySpi = embassy_stm32::spi::Spi<'static, SPI1, DMA1_CH1, DMA1_CH2>;
+pub type EmbassySpi<S: Peripheral> = embassy_stm32::spi::Spi<'static, S, DMA1_CH1, DMA1_CH2>;
 
-pub struct SpiPins {
-    pub spi: SPI1,
+pub struct SpiPins<S: Peripheral> {
+    pub spi: S,
     pub sck: PA5,
     pub miso: PA6,
     pub mosi: PA7,
@@ -12,16 +13,16 @@ pub struct SpiPins {
     pub dma_rx: DMA1_CH2,
 }
 
-pub struct Spi {
-    embassy_spi: EmbassySpi
+pub struct Spi<S: Peripheral> {
+    embassy_spi: EmbassySpi<S>
 }
 
-impl Spi {
-    pub fn new(pins: SpiPins) -> Self {
+impl <S: Peripheral> Spi<S> {
+    pub fn new(pins: SpiPins<S>) -> Self {
         Self::new_from_config(pins, em_spi::Config::default())
     }
 
-    pub fn new_from_config(pins: SpiPins, config: em_spi::Config) -> Self {
+    pub fn new_from_config(pins: SpiPins<S>, config: em_spi::Config) -> Self {
         Self {
             embassy_spi: EmbassySpi::new(
                 pins.spi,
