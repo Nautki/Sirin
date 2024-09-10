4d282b914fe6bac3a3320c1a1d8ece236389884b
Serena <23511780+SerenaLynas@users.noreply.github.com>
2024-09-10 18:52:05 -0400

    stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,158 @@
 #![no_std]
 
-use core::future::Future;
-use embedded_hal_async::spi::SpiBus;
+use core::{borrow::Borrow, future::Future, marker::PhantomData, ops::{Deref, DerefMut}};
+use embedded_hal_async::spi::{ErrorType, SpiBus};
 
-pub trait SpiHandle {
-    /// The awaited return type MUST implement [Drop] to deselect the SPI bus.
-    /// 
+pub trait SpiHandle<W: 'static + Copy = u8> {
+    /// The type of the actual SPI bus we're using.
+    type Bus: SpiBus<W>;
+
     /// The future resolves once the SPI device has been selected on the bus. 
     /// This method is infallible. It should not panic or fail. If the 
-    /// device cannot be selected, the future should wait until it can
-    /// be selected, then it should select it and resolve.
+    /// device cannot be selected, the future should wait intil it is able to select.
+    fn select(&mut self) -> impl Future<Output = SpiHandleBus<Self, W>>;
+
+    /// Deselects the chip. You'll implement this here, but you shouldn't call
+    /// this from your code -- the chip will automatically be deselected when
+    /// [SpiHandleBus] is dropped. You can also call [SpiHandleBus::deselect],
+    /// to drop manually.
     /// 
-    /// The returned [SpiBus] must guarantee exclusive access.
-    fn select<'a>(&'a mut self) -> impl Future<Output = impl SpiBus + 'a>;
+    /// You can access self through `bus.handle()`.
+    fn deselect(bus: &mut SpiHandleBusDestructor<Self, W>);
 }
+
+pub struct SpiHandleBus<'h, H: 'h + SpiHandle<W> + ?Sized, W: 'static + Copy> {
+    handle: &'h mut H,
+    bus: H::Bus
+}
+
+impl <'h, H: SpiHandle<W> + ?Sized, W: 'static + Copy> SpiHandleBus<'h, H, W> {
+    pub fn new(handle: &'h mut H, bus: H::Bus) -> Self {
+        SpiHandleBus {
+            handle,
+            bus
+        }
+    }
+
+    /// Deselects the chip (by dropping self)
+    /// This is the same as just dropping it yourself, but the name
+    /// makes your code more readable.
+    pub fn deselect(self) {
+        // drops it lol
+    }
+}
+
+impl <'h, H: 'h + SpiHandle<W> + ?Sized, W: 'static + Copy> Drop for SpiHandleBus<'h, H, W> {
+    fn drop(&mut self) {
+        H::deselect(&mut SpiHandleBusDestructor {
+            handle: self.handle,
+            _phantom: PhantomData
+        });
+    }
+}
+
+pub struct SpiHandleBusDestructor<'h, H: 'h + SpiHandle<W> + ?Sized, W: 'static + Copy> {
+    handle: &'h mut H,
+    _phantom: PhantomData<W>
+}
+
+impl <H: SpiHandle<W> + ?Sized, W: 'static + Copy> SpiHandleBusDestructor<'_, H, W> {
+    pub fn handle(&mut self) -> &mut H {
+        &mut self.handle
+    }
+}
+
+impl <H: SpiHandle<W> + ?Sized, W: 'static + Copy> Deref for SpiHandleBus<'_, H, W> {
+    type Target = H::Bus;
+
+    fn deref(&self) -> &Self::Target {
+        &self.bus
+    }
+}
+
+impl <H: SpiHandle<W> + ?Sized, W: 'static + Copy> DerefMut for SpiHandleBus<'_, H, W> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.bus
+    }
+}
+
+impl <H: SpiHandle<W> + ?Sized, W: 'static + Copy> ErrorType for SpiHandleBus<'_, H, W> {
+    type Error = <H::Bus as ErrorType>::Error;
+}
+
+impl <'h, H: SpiHandle<W>, W: 'static + Copy> SpiBus<W> for SpiHandleBus<'h, H, W> {
+    async fn read(&mut self, words: &mut [W]) -> Result<(), Self::Error> {
+        self.bus.read(words).await
+    }
+
+    async fn write(&mut self, words: &[W]) -> Result<(), Self::Error> {
+        self.bus.write(words).await
+    }
+
+    async fn transfer(&mut self, read: &mut [W], write: &[W]) -> Result<(), Self::Error> {
+        self.bus.transfer(read, write).await
+    }
+
+    async fn transfer_in_place(&mut self, words: &mut [W]) -> Result<(), Self::Error> {
+        self.bus.transfer_in_place(words).await
+    }
+
+    async fn flush(&mut self) -> Result<(), Self::Error> {
+        self.bus.flush().await
+    }
+}
+
+pub struct DerefSpiBus<D: DerefMut>(pub D)
+where D::Target: SpiBus;
+
+impl <D: DerefMut> ErrorType for DerefSpiBus<D>
+where D::Target: SpiBus {
+    type Error = <D::Target as ErrorType>::Error;
+}
+
+impl <D: DerefMut> Deref for DerefSpiBus<D>
+where D::Target: SpiBus {
+    type Target = D;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl <D: DerefMut> DerefMut for DerefSpiBus<D>
+where D::Target: SpiBus {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+type W = u8;
+
+impl <D: DerefMut> SpiBus for DerefSpiBus<D>
+where D::Target: SpiBus {
+    async fn read(&mut self, words: &mut [W]) -> Result<(), Self::Error> {
+        (**self).read(words).await
+    }
+
+    async fn write(&mut self, words: &[W]) -> Result<(), Self::Error> {
+        (**self).write(words).await
+    }
+
+    async fn transfer(&mut self, read: &mut [W], write: &[W]) -> Result<(), Self::Error> {
+        (**self).transfer(read, write).await
+    }
+
+    async fn transfer_in_place(&mut self, words: &mut [W]) -> Result<(), Self::Error> {
+        (**self).transfer_in_place(words).await
+    }
+
+    async fn flush(&mut self) -> Result<(), Self::Error> {
+        (**self).flush().await
+    }
+}
+
+/*
+async fn test(mut handle: impl SpiHandle) {
+    let mut b = handle.select().await;
+    
+}*/
