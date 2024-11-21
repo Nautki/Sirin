53b11f647e8b5e47775df76d8f6098375d00feed
DrYogurt <ben.z.rubinstein@gmail.com>
2024-11-21 18:33:01 -0500

    Added the changes that Ella made  	modified:   lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,44 @@
-use std::future::IntoFuture;
-
+use std::{future::IntoFuture, u32};
 use embedded_hal_async::spi::ErrorKind;
 use spi_handle::SpiHandle;
 use embedded_hal_async::spi::SpiBus;
-
 pub struct W25Q<S: SpiHandle>{
     spi: S
 }
-
-pub struct Log{
-    current_index : usize
+pub struct Logger {
+    current_addr: u32
 }
-
+impl Logger {
+    async fn log_partial_page(&mut self, flash: &mut W25Q<impl SpiHandle>, msg: &[u8]) -> Result<(), ErrorKind> {
+        if self.current_addr % 4096 == 0 {
+            flash.sector_erase(self.current_addr);
+        }
+        flash.page(self.current_addr, msg).await?;
+        self.current_addr += msg.len() as u32;
+        Ok(())
+    }
+    async fn log(&mut self, flash: &mut W25Q<impl SpiHandle>, msg: &[u8]) -> Result<(), ErrorKind> {
+        let start = self.current_addr as usize;
+        let end = self.current_addr as usize + msg.len();
+        let mut msg_i = 0;
+        let first_full_page = ((self.current_addr as usize / 256) + 1) * 256;
+        self.log_partial_page(flash, &msg[..(first_full_page - start)]).await?;
+        msg_i = first_full_page - start;
+        while ((self.current_addr as usize / 256) + 1) * 256 <= end {
+            self.log_partial_page(flash, &msg[msg_i..(msg_i + 256)]).await?;
+            msg_i += 256;
+        }
+        self.log_partial_page(flash, &msg[msg_i..]).await?;
+        Ok(())
+    }
+}
 impl <S: SpiHandle> W25Q<S>{
-
     //write enable
     pub async fn write_enable(&mut self) -> Result<(), ErrorKind> {
         let mut spi = self.spi.select().await;
         spi.write(&[0x06]).await?;
         Ok(())
     }
-
     //page program 02h
     pub async fn page(&mut self, addr: u32, words: &[u8]) -> Result<(), ErrorKind> {
         let mut spi = self.spi.select().await;
@@ -29,15 +47,13 @@ impl <S: SpiHandle> W25Q<S>{
         spi.write(words).await?;
         Ok(())
     }
-
-    //sector erase 20h
+    /// sector erase 20h
     pub async fn sector_erase(&mut self, addr: u32) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
         spi.write(&[0x20]).await?;
         spi.write(&[((addr >> 16) & 0xFF) as u8, ((addr >> 8) & 0xFF) as u8, ((addr & 0xFF) as u8)]).await?;
         Ok(())
     }
-
     //read data 03h
     pub async fn read_data(&mut self, addr: u32, words: &mut[u8]) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
@@ -46,7 +62,6 @@ impl <S: SpiHandle> W25Q<S>{
         spi.read(words).await?;
         Ok(())
     }
-
     //all non-fast read, writes, erases should be implemented
     //chip erase
     pub async fn chip_erase(&mut self) -> Result<(), ErrorKind>{
@@ -54,7 +69,6 @@ impl <S: SpiHandle> W25Q<S>{
         spi.write(&[0x60]).await?;
         Ok(())
     }
-
     //block erase 64KB D8h
     pub async fn block_64kb_erase(&mut self, addr:u32) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
@@ -62,7 +76,6 @@ impl <S: SpiHandle> W25Q<S>{
         spi.write(&[((addr >> 16) & 0xFF) as u8, ((addr >> 8) & 0xFF) as u8, ((addr & 0xFF) as u8)]).await?;
         Ok(())
     }
-
     //block erase 32KB 52h
     pub async fn block_32kb_erase(&mut self, addr:u32) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
@@ -70,52 +83,29 @@ impl <S: SpiHandle> W25Q<S>{
         spi.write(&[((addr >> 16) & 0xFF) as u8, ((addr >> 8) & 0xFF) as u8, ((addr & 0xFF) as u8)]).await?;
         Ok(())
     }
-
     //erase/program suspend 75h
     pub async fn suspend(&mut self) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
         spi.write(&[0x75]).await?;
         Ok(())
     }
-
     //erase/program resume 7Ah
     pub async fn resume(&mut self) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
         spi.write(&[0x7A]).await?;
         Ok(())
     }
-
-
     //read device_id 90h
     pub async fn read_device_id(&mut self) -> Result<u8, ErrorKind>{
         let mut spi = self.spi.select().await;
         spi.write(&[0x90]).await?;
         spi.write(&[0,0,0]).await?;
         let mut array: [u8; 2] = [0; 2];
-        spi.read(&mut array).await?;  
+        spi.read(&mut array).await?;
         Ok(array[1])
     }
-
     //read manufacturer id --returns a constant EFh
     pub fn read_manufacturer_id(&mut self) -> u16{
         0xEF
     }
-
-    //write to block with a block pointer(point to last erase / the current block writing to)
-    //wrap around when reaches the end.
-    pub async fn log(&mut self, arr: &mut[u8], words: &[u8] ,current_index : *mut u8) -> Result<(),ErrorKind>{
-        let mut spi = self.spi.select().await;
-        unsafe { current_index.add(words.len())};
-        let last_ptr = unsafe { arr.as_mut_ptr().add(arr.len()) };
-
-        if current_index > last_ptr{
-            current_index = arr.as_mut_ptr() //we set the current to the beginning = 0
-        }
-        self.sector_erase(*current_index); //sector_erase takes u32, do we erase 4 sets of the index??
-        Ok(())
-    }
-    
-    
-    }
-
-    
+}   
