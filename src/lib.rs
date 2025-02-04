a00f3e8b71dd8065e688eec2e127bef376e3172a
Serena <23511780+SerenaLynas@users.noreply.github.com>
2025-02-04 18:54:32 -0500

    Fix flash chip

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 #![no_std]
 
+use core::{future::{poll_fn, Future}, task::Poll};
+
 use embedded_hal_async::spi::ErrorKind;
 use spi_handle::SpiHandle;
 use embedded_hal_async::spi::SpiBus;
+
+mod yield_now;
+
 pub struct W25Q<S: SpiHandle>{
     spi: S
 }
@@ -13,20 +18,55 @@ impl <S: SpiHandle> W25Q<S>{
             spi
         }
     }
+
+    pub async fn read_status_reg_1(&mut self) -> Result<u8, ErrorKind> {
+        let mut spi = self.spi.select().await;
+        let mut out = [0u8];
+        spi.write(&[0x05]).await?;
+        spi.read(&mut out).await?;
+        Ok(out[0])
+    }
+
+    pub async fn is_busy(&mut self) -> Result<bool, ErrorKind> {
+        Ok(self.read_status_reg_1().await? % 2 == 1)
+    }
+
+    /// Returns a future that completes when the chip is ready (not BUSY)
+    pub async fn until_ready(&mut self) -> Result<(), ErrorKind> {
+        while self.is_busy().await? {
+            yield_now::yield_now().await;
+        }
+
+        Ok(())
+    }
+
     //write enable
     pub async fn write_enable(&mut self) -> Result<(), ErrorKind> {
         let mut spi = self.spi.select().await;
         spi.write(&[0x06]).await?;
+
         Ok(())
     }
+
+    // TODO: mutex/lockout to handle concurrent pages?
+    pub async fn prepare_write(&mut self) -> Result<(), ErrorKind> {
+        self.until_ready().await?;
+        self.write_enable().await?;
+
+        Ok(())
+    }
+
     //page program 02h
     pub async fn page(&mut self, addr: u32, words: &[u8]) -> Result<(), ErrorKind> {
+        self.prepare_write().await?;
+
         let mut spi = self.spi.select().await;
         spi.write(&[0x02]).await?;
         spi.write(&[((addr >> 16) & 0xFF) as u8, ((addr >> 8) & 0xFF) as u8, ((addr & 0xFF) as u8)]).await?;
         spi.write(words).await?;
         Ok(())
     }
+
     /// sector erase 20h
     pub async fn sector_erase(&mut self, addr: u32) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
@@ -34,21 +74,29 @@ impl <S: SpiHandle> W25Q<S>{
         spi.write(&[((addr >> 16) & 0xFF) as u8, ((addr >> 8) & 0xFF) as u8, ((addr & 0xFF) as u8)]).await?;
         Ok(())
     }
+
     //read data 03h
     pub async fn read_data(&mut self, addr: u32, words: &mut[u8]) -> Result<(), ErrorKind>{
+        self.until_ready().await?;
+
         let mut spi = self.spi.select().await;
         spi.write(&[0x03]).await?;
         spi.write(&[((addr >> 16) & 0xFF) as u8, ((addr >> 8) & 0xFF) as u8, ((addr & 0xFF) as u8)]).await?;
         spi.read(words).await?;
         Ok(())
     }
+
     //all non-fast read, writes, erases should be implemented
     //chip erase
     pub async fn chip_erase(&mut self) -> Result<(), ErrorKind>{
+        self.prepare_write().await?;
+        
         let mut spi = self.spi.select().await;
         spi.write(&[0x60]).await?;
+        // TODO: await erase
         Ok(())
     }
+
     //block erase 64KB D8h
     pub async fn block_64kb_erase(&mut self, addr:u32) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
@@ -56,6 +104,7 @@ impl <S: SpiHandle> W25Q<S>{
         spi.write(&[((addr >> 16) & 0xFF) as u8, ((addr >> 8) & 0xFF) as u8, ((addr & 0xFF) as u8)]).await?;
         Ok(())
     }
+
     //block erase 32KB 52h
     pub async fn block_32kb_erase(&mut self, addr:u32) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
@@ -63,18 +112,21 @@ impl <S: SpiHandle> W25Q<S>{
         spi.write(&[((addr >> 16) & 0xFF) as u8, ((addr >> 8) & 0xFF) as u8, ((addr & 0xFF) as u8)]).await?;
         Ok(())
     }
+
     //erase/program suspend 75h
     pub async fn suspend(&mut self) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
         spi.write(&[0x75]).await?;
         Ok(())
     }
+
     //erase/program resume 7Ah
     pub async fn resume(&mut self) -> Result<(), ErrorKind>{
         let mut spi = self.spi.select().await;
         spi.write(&[0x7A]).await?;
         Ok(())
     }
+
     //read device_id 90h
     pub async fn read_device_id(&mut self) -> Result<u8, ErrorKind>{
         let mut spi = self.spi.select().await;
@@ -84,10 +136,14 @@ impl <S: SpiHandle> W25Q<S>{
         spi.read(&mut array).await?;
         Ok(array[1])
     }
+
+    /*
+    TODO: Fix
+    
     //read manufacturer id --returns a constant EFh
     pub fn read_manufacturer_id(&mut self) -> u16{
         0xEF
-    }
+    }*/
 }   
 
 pub struct Logger {
